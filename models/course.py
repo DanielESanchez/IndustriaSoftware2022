b6949faab5404a42a3dff7f03e43c071a051a4cb
Daniel Sanchez <dsanchezz@unah.hn>
2022-02-21 12:21:23 -0600

    Creacion de vista basica Course

---
+++ models/course.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class course(models.Model):
+    _name = 'open_academy.course'
+    _description = 'Course'
+
+    name = fields.Char(string="Course")
+    title = fields.Char(string="Title", required=True)
+    description = fields.Text(string="Description")
